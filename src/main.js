20817e2b5b817f68d5331c3769b6ef2d01743dec
TiaanSM <108478468+TiaanSM@users.noreply.github.com>
2022-10-19 17:06:36 +0200

    Fixed bug on mobile

--- src/main.js
+++ src/main.js
@@ -31,3 +31,18 @@ document.addEventListener('scroll', () => {
   checkIsVisible(trolleySection);
 });
 
+/* Image changes
+let images = ['photoFromInternet', 'photoFromInternet2', 'photoFromInternet3'];
+
+let index = 0;
+const imgElement = document.querySelector('#mainPhoto');
+
+function change() {
+   imgElement.src = images[index];
+   index > 1 ? index = 0 : index++;
+}
+
+window.onload = function () {
+    setInterval(change, 5000);
+};
+*/
